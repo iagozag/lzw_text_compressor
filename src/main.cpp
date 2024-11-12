7b872fe74857ea46cdb5428839b3e57510ef5df7
iagozag <iagozag@gmail.com>
2024-11-12 15:19:32 -0300

    diretorios base criados

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+
+int main(){ _
+	cout << "Hello World" << endl;
+
+    exit(0);
+}
