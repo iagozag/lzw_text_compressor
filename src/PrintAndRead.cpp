7f4f7a009abc9d10fcad3f8d6eff092bbcbd04a1
Manuel Ferraz <128011243+manuelferrazc@users.noreply.github.com>
2024-11-18 04:57:12 -0300

    Update PrintAndRead.cpp

--- src/PrintAndRead.cpp
+++ src/PrintAndRead.cpp
@@ -1,4 +1,6 @@
 #include "../include/PrintAndRead.h"
+#include <algorithm>
+#include <fstream>
 
 Print::Print() {}
 
@@ -14,31 +16,57 @@ void Print::add_bits(string s,int z) {
 }
 
 void Print::print(ofstream &f, int n) {
-    f.write((char*)&n,4);
+    if(not buf.size()) return;
 
+    f.write((char*)&n,4);
+    n = buf.size()/8;
+    if(buf.size()%8) n++;
+    f.write((char*)&n,4);
+    
+    string s;
     char c=0;
     int i=8;
     for(auto b: buf) {
         i--;
         c|=(b<<i);
         if(not i) {
-            f.write(&c,1);
+            s.push_back(c);
             i = 8;
             c = 0;
         }
     }
-    if(i!=8) f.write(&c,1);
+    if(i!=8) s.push_back(c);
+    f.write(&s[0],s.size());
     buf.clear();
 }
 
+void Print::print(ofstream &f) {
+    string s;
+    reverse(buf.begin(),buf.end());
+    char c;
+    while(buf.size()) {
+        for(int i=0;i<8 and buf.size();i++) {
+            c<<=1;
+            c |= buf.back();
+            buf.pop_back();
+        }
+        s.push_back(c);
+    }
 
+    f.write(&s[0],s.size());
+}
 void Read(ifstream &f, vector<string> &codes) {
-    int n;
+    int n=0;
     f.read((char*)&n,4);
-    int sz = (1<<n)-256;
+    if(not n) return;
+    int sz;
+    f.read((char*)&sz,4);
+    char str[sz];
     char c;
     string s;
-    while((int)codes.size()<sz and f.read(&c,1)) {
+    f.read(str,sz);
+    for(int q = 0;q<sz;q++) {
+        c = str[q];
         int i=8;
         while(i--) {
             if((int)s.size()==n) {
